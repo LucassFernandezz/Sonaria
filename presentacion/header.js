ce8ca4d405bfb83f2b6a765a16c0f44dcd613f9f
LucasF <lucas.fernandezet36@gmail.com>
2025-12-03 20:47:49 -0300

    Ademas de login funcional, Ahora tambien se añadio el menu personalizado segun rol, y mejoro el estilo de la pagina

---
+++ presentacion/header.js
@@ -0,0 +1,64 @@
+// ==========================
+// Cargar el header
+// ==========================
+async function cargarHeader() {
+    const cont = document.getElementById("header-container");
+    if (!cont) return;
+
+    const resp = await fetch("header.html");
+    const html = await resp.text();
+    cont.innerHTML = html;
+
+    configurarHeader();
+}
+
+cargarHeader();
+
+
+// ==========================
+// Lógica del header dinámico
+// ==========================
+async function configurarHeader() {
+
+    // ---- botones ----
+    const btnLogin = document.getElementById("btn-login");
+    const btnRegister = document.getElementById("btn-register");
+
+    const btnSubir = document.getElementById("btn-subir");
+    const btnPerfil = document.getElementById("btn-perfil");
+    const btnSettings = document.getElementById("btn-settings");
+
+    const btnAdmin = document.getElementById("btn-admin");
+
+    // Consulta al backend
+    const resp = await fetch("http://localhost:5000/auth/me", {
+        credentials: "include"
+    });
+
+    const data = await resp.json();
+
+
+    // ==========================
+    // Visitante (no logueado)
+    // ==========================
+    if (!data.autenticado) {
+        btnLogin.classList.remove("oculto");
+        btnRegister.classList.remove("oculto");
+        return; // No mostramos más
+    }
+
+
+    // ==========================
+    // Usuario logueado
+    // ==========================
+    btnSubir.classList.remove("oculto");
+    btnPerfil.classList.remove("oculto");
+    btnSettings.classList.remove("oculto");
+
+    // ==========================
+    // Es ADMIN
+    // ==========================
+    if (data.rol === "admin") {
+        btnAdmin.classList.remove("oculto");
+    }
+}
